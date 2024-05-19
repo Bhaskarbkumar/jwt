7d3de98153372506a63b659ae3817f5513b0023c
bharath <bhaskarbkumar@gmail.com>
2024-05-18 19:12:34 -0500

    commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bhaskarbkumar/jwt
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
